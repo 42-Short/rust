27d990d6b741c88e62ae5c8ed506baafa6f72800
winstonallo <arthurbiedchar@gmail.com>
2024-08-05 20:19:54 +0200

    fix: remove unused arguments from shortinette.Init

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"rust-piscine/internal/tests/R00"
 
 	"github.com/42-Short/shortinette"
+	Short "github.com/42-Short/shortinette/pkg/short"
 	Module "github.com/42-Short/shortinette/pkg/interfaces/module"
 )
 
 func main() {
-	// short := Short.NewShort("Rust Piscine 1.0", modules, webhook.NewWebhookTestMode(modules["00"]))
+	shortinette.Init()
+	short := Short.NewShort("Rust Piscine 1.0", map[string]Module.Module{"00": *R00.R00()}, webhook.NewWebhookTestMode(modules["00"]))
 
-	shortinette.Init(map[string]Module.Module{"00": *R00.R00()})
 }
