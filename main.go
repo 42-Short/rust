b50a9c17e9e2700a9254fc5ebcbba081fd2af50e
winstonallo <arthurbiedchar@gmail.com>
2024-08-06 07:11:58 +0200

    add: now use shortinettev0.1.5-alpha

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"rust-piscine/internal/tests/R00"
 
 	"github.com/42-Short/shortinette"
-	Module "github.com/42-Short/shortinette/pkg/interfaces/module"
 	"github.com/42-Short/shortinette/pkg/short/testmodes/webhooktestmode"
+	Module "github.com/42-Short/shortinette/pkg/interfaces/module"
 	Short "github.com/42-Short/shortinette/pkg/short"
 )
 
 func main() {
 	shortinette.Init()
-	short := Short.NewShort("Rust Piscine 1.0", map[string]Module.Module{"00": *R00.R00()}, nil)
-	short.TestMode = webhook.NewWebhookTestMode(short.Modules["00"])
+	modules := map[string]Module.Module{
+		"00": *R00.R00(),
+	}
+	short := Short.NewShort("Rust Piscine 1.0", modules, webhook.NewWebhookTestMode(modules))
 	shortinette.Start(short, "00")
 }
