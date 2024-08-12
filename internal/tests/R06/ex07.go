6c4a1f27cd745b316c3dce65616e1c7a82ce9b6e
winstonallo <arthurbiedchar@gmail.com>
2024-08-12 18:49:08 +0200

    add: (not yet working) tests for ex07

--- internal/tests/R06/ex07.go
+++ internal/tests/R06/ex07.go
@@ -1,9 +1,35 @@
 package R06
 
-import Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+	"github.com/42-Short/shortinette/pkg/testutils"
+)
 
 func ex07Test(exercise *Exercise.Exercise) Exercise.Result {
-	return Exercise.Passed("NOT IMPLEMENTED")
+	fullFilePath := filepath.Join(exercise.RepoDirectory, exercise.TurnInDirectory, exercise.TurnInFiles[0])
+	if output, err := testutils.RunCommandLine(filepath.Dir(fullFilePath), "rustc", []string{"-C panic=abort", "-C link-args=-nostartfiles", "-o ft_putchar", "ft_putchar.rs"}); err != nil {
+		return Exercise.CompilationError(fmt.Sprintf("%s: %s", err.Error(), output))
+	}
+	executablePath := strings.TrimSuffix(exercise.TurnInFiles[0], ".rs")
+
+	output, err := testutils.RunExecutable(executablePath)
+	if err != nil {
+		return Exercise.RuntimeError(err.Error())
+	}
+	
+	if output != "42" {
+		return Exercise.AssertionError("42", output)
+	}
+
+	if output, _ := testutils.RunCommandLine(".", "echo", []string{"$?"}); output != "42" {
+		return Exercise.RuntimeError(fmt.Sprintf("expected exit code '42', got '%s'", output))
+	}
+ 
+	return Exercise.Passed("OK")
 }
 
 func ex07() Exercise.Exercise {
