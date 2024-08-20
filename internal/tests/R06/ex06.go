89d7c5105afb4a26f5697bf830bf514edcfc9871
winstonallo <arthurbiedchar@gmail.com>
2024-08-20 21:46:23 +0200

    add: disallowed methods and types for ex06

--- internal/tests/R06/ex06.go
+++ internal/tests/R06/ex06.go
@@ -2,6 +2,7 @@ package R06
 
 import (
 	"path/filepath"
+	"rust-piscine/internal/alloweditems"
 
 	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
 	"github.com/42-Short/shortinette/pkg/logger"
@@ -100,7 +101,15 @@ mod shortinette_tests_rust_0605 {
 }
 `
 
+var clippyTomlAsString06 = `
+disallowed-types = ["std::collections::Vec", "std::collections::VecDeque", "std::collections::LinkedList", "Box<T>", "Rc<T>", "Arc<T>", "std::cell::RefCell", "std::sync::Mutex", "std::mem::ManuallyDrop", "std::alloc::System"]
+disallowed-methods = ["std::ptr::copy", "std::ptr::write", "std::ptr::replace", "std::mem::transmute"]
+`
+
 func ex06Test(exercise *Exercise.Exercise) Exercise.Result {
+	if err := alloweditems.Check(*exercise, clippyTomlAsString06, "#![allow(clippy::needless_borrows_for_generic_args)]"); err != nil {
+		return Exercise.CompilationError(err.Error())
+	}
 	workingDirectory := filepath.Join(exercise.RepoDirectory, exercise.TurnInDirectory)
 	if err := testutils.AppendStringToFile(tests06, exercise.TurnInFiles[0]); err != nil {
 		logger.Exercise.Printf("could not write to %s: %v", exercise.TurnInFiles[0], err)
