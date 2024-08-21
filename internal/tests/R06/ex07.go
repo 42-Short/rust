54e11c91c9626c1620fcb5c2cd5717ba824d8faa
winstonallo <arthurbiedchar@gmail.com>
2024-08-21 10:45:36 +0200

    add: checker for attributes in rust files

--- internal/tests/R06/ex07.go
+++ internal/tests/R06/ex07.go
@@ -2,36 +2,20 @@ package R06
 
 import (
 	"fmt"
-	"os"
 	"path/filepath"
+	"rust-piscine/internal/attributes"
 	"strings"
 
 	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
 	"github.com/42-Short/shortinette/pkg/testutils"
 )
 
-func checkGlobalAttributesUsage(exercise *Exercise.Exercise) (err error) {
-	fileAsBytes, err := os.ReadFile(filepath.Join(exercise.RepoDirectory, exercise.TurnInDirectory, "ft_putchar.rs"))
-	if err != nil {
-		return err
-	}
-	missingAttributes := map[string]bool{
+func ex07Test(exercise *Exercise.Exercise) Exercise.Result {
+	requiredAttributes := map[string]bool{
 		"#![no_std]":  true,
 		"#![no_main]": true,
 	}
-	fileAsSlice := strings.Split(string(fileAsBytes), "\n")
-
-	for _, line := range fileAsSlice {
-		delete(missingAttributes, line)
-	}
-	if len(missingAttributes) != 0 {
-		return fmt.Errorf("missing global attributes: %v", missingAttributes)
-	}
-	return nil
-}
-
-func ex07Test(exercise *Exercise.Exercise) Exercise.Result {
-	if err := checkGlobalAttributesUsage(exercise); err != nil {
+	if err := attributes.Check(filepath.Join(exercise.RepoDirectory, exercise.TurnInDirectory, "ft_putchar.rs"), requiredAttributes, map[string]bool{}); err != nil {
 		return Exercise.CompilationError(err.Error())
 	}
 	if output, err := testutils.RunCommandLine(filepath.Dir(exercise.TurnInFiles[0]), "rustc", []string{"-C", "panic=abort", "-C", "link-args=-nostartfiles", "-o", "ft_putchar", "ft_putchar.rs"}); err != nil {
