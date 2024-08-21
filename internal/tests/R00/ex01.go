aaf9d97b5fc45dd453b6d99442d431b3f2accb49
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2024-08-21 19:20:18 +0200

    Explicitly specify the data types in the ex01 test, rename test module

--- internal/tests/R00/ex01.go
+++ internal/tests/R00/ex01.go
@@ -14,27 +14,27 @@ import (
 
 const CargoTest = `
 #[cfg(test)]
-mod tests {
+mod shortinette_tests_rust_0001 {
     use super::*;
 
     #[test]
     fn test_0() {
-        assert_eq!(min(1, 2), 1);
+        assert_eq!(min(1i32, 2i32), 1i32);
     }
 
     #[test]
     fn test_1() {
-        assert_eq!(min(2, 1), 1);
+        assert_eq!(min(2i32, 1i32), 1i32);
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(min(1, 1), 1);
+        assert_eq!(min(1i32, 1i32), 1i32);
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(min(-1, 0), -1);
+        assert_eq!(min(-1i32, 0i32), -1i32);
     }
 }
 `
