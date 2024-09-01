5a7e1f061085a3bbffed6315fef459f770538e4d
winstonallo <arthurbiedchar@gmail.com>
2024-09-01 16:27:26 +0200

    add: ex06 tests

--- internal/tests/R04/ex06.go
+++ internal/tests/R04/ex06.go
@@ -1,9 +1,48 @@
 package R04
 
-import Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"rust-piscine/internal/alloweditems"
 
-func ex06Test(exercise *Exercise.Exercise) Exercise.Result {
-	return Exercise.InternalError("NOT IMPLEMENTED")
+	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+	"github.com/42-Short/shortinette/pkg/testutils"
+)
+
+var clippyTomlAsString06 = ``
+
+func testSimpleCBinary(workingDirectory string) Exercise.Result {
+	if err := os.WriteFile(filepath.Join(workingDirectory, "test.c"), []byte("int main(){return 0;}"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if _, err := testutils.RunCommandLine(workingDirectory, "cc", []string{filepath.Join(workingDirectory, "test.c")}); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	commandLine := []string{"cargo", "run", filepath.Join(workingDirectory, "a.out")}
+	output, err := testutils.RunCommandLine(workingDirectory, commandLine[0], commandLine[:1])
+	if err != nil {
+		return Exercise.RuntimeError(err.Error())
+	}
+	fmt.Println(output)
+	return Exercise.Passed("OK")
+}
+
+func ex06Test(exercise *Exercise.Exercise) (result Exercise.Result) {
+	if err := alloweditems.Check(*exercise, clippyTomlAsString06, nil); err != nil {
+		return Exercise.CompilationError(err.Error())
+	}
+	workingDirectory := filepath.Join(exercise.CloneDirectory, exercise.TurnInDirectory)
+
+	if result = testNoInput(workingDirectory); !result.Passed {
+		return result
+	}
+	if result = testSimpleCBinary(workingDirectory); !result.Passed {
+		return result
+	}
+
+	return Exercise.Passed("OK")
 }
 
 func ex06() Exercise.Exercise {
