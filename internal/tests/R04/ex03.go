71d76f322ab56a63b8ab728f2b2f4d35c3407971
winstonallo <arthurbiedchar@gmail.com>
2024-09-01 10:10:52 +0200

    add: tests for ex03

--- internal/tests/R04/ex03.go
+++ internal/tests/R04/ex03.go
@@ -1,9 +1,99 @@
 package R04
 
-import Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"rust-piscine/internal/alloweditems"
+	"strings"
 
-func ex03Test(exercise *Exercise.Exercise) Exercise.Result {
-	return Exercise.InternalError("NOT IMPLEMENTED")
+	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
+	"github.com/42-Short/shortinette/pkg/testutils"
+)
+
+var clippyTomlAsString03 = ``
+
+func testInputFileBadPermissions(workingDirectory string) Exercise.Result {
+	if _, err := testutils.RunCommandLine(workingDirectory, "sh", []string{"-c", "chmod -R 777 target && touch donotpanic && chmod 000 donotpanic"}); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if _, err := testutils.RunCommandLine(workingDirectory, "sh", []string{"-c", "su -c 'echo donotpanic | cargo run -- cat'"}); err != nil {
+		if strings.Contains(err.Error(), "panicked") {
+			return Exercise.RuntimeError(err.Error(), "touch donotpanic", "chmod 000 donotpanic", "echo donotpanic | cargo run -- cat")
+		}
+	}
+	return Exercise.Passed("OK")
+}
+
+func testHelloWorld(workingDirectory string) Exercise.Result {
+	commandLine := "echo 'Hello, World!' | cargo run -- echo"
+	expectedOutput := "Hello, World!\n"
+
+	return doTest(workingDirectory, expectedOutput, commandLine)
+}
+
+func testCatMultipleFiles(workingDirectory string) Exercise.Result {
+	if err := os.WriteFile("/tmp/a", []byte("a"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if err := os.WriteFile("/tmp/b", []byte("b"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if err := os.WriteFile("/tmp/c", []byte("c"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if err := os.WriteFile("/tmp/d", []byte("d"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+	if err := os.WriteFile("/tmp/e", []byte("e"), fs.FileMode(os.O_WRONLY)); err != nil {
+		return Exercise.InternalError(err.Error())
+	}
+
+	commandLine := "<< EOF cargo run -- cat\n/tmp/a\n/tmp/b\n/tmp/c\n/tmp/d\n/tmp/e\n"
+	expectedOutput := "abcde"
+
+	return doTest(workingDirectory, expectedOutput, commandLine)
+}
+
+func testMultiLineInput(workingDirectory string) Exercise.Result {
+	commandLine := "<< EOF cargo run -- echo -n\nHello\n,\nWorld\n!\nEOF"
+	expectedOutput := "Hello , World !"
+
+	return doTest(workingDirectory, expectedOutput, commandLine)
+}
+
+func ex03Test(exercise *Exercise.Exercise) (result Exercise.Result) {
+	if err := alloweditems.Check(*exercise, clippyTomlAsString03, nil); err != nil {
+		return Exercise.CompilationError(err.Error())
+	}
+
+	workingDirectory := filepath.Join(exercise.CloneDirectory, exercise.TurnInDirectory)
+
+	if result = testInputFileBadPermissions(workingDirectory); !result.Passed {
+		return result
+	}
+	if result = testHelloWorld(workingDirectory); !result.Passed {
+		return result
+	}
+	if result = testMultiLineInput(workingDirectory); !result.Passed {
+		return result
+	}
+	if result = testCatMultipleFiles(workingDirectory); !result.Passed {
+		return result
+	}
+
+	return Exercise.Passed("OK")
+}
+
+func doTest(workingDirectory string, expectedOutput string, commandLine string) Exercise.Result {
+	output, err := testutils.RunCommandLine(workingDirectory, "sh", []string{"-c", commandLine})
+	if err != nil {
+		return Exercise.RuntimeError(err.Error(), commandLine)
+	}
+	if output != expectedOutput {
+		return Exercise.AssertionError(expectedOutput, output, commandLine)
+	}
+	return Exercise.Passed("OK")
 }
 
 func ex03() Exercise.Exercise {
