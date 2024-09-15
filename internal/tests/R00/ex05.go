4b4e26a06bd8abccdcb9d1e8c92ef2936cb42ef5
Bernhard Gebetsberger <bgebetsb@student.42vienna.com>
2024-09-15 16:21:49 +0000

    Fix r00/ex05 test (only slightly over-engineered)

--- internal/tests/R00/ex05.go
+++ internal/tests/R00/ex05.go
@@ -1,19 +1,41 @@
 package R00
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"regexp"
 	"rust-piscine/internal/alloweditems"
+	"rust-piscine/internal/cargo"
 	"strconv"
 	"strings"
 	"time"
 
 	Exercise "github.com/42-Short/shortinette/pkg/interfaces/exercise"
-	"github.com/42-Short/shortinette/pkg/logger"
 	"github.com/42-Short/shortinette/pkg/testutils"
 )
+
+const TestExistenceHelper = `
+use std::os::unix::net::UnixStream;
+use std::io::prelude::*;
+
+fn is_leap_year_helper(year: u32) {
+	let text = format!("is_leap_year({});\n", year);
+	if let Ok(mut stream) = UnixStream::connect("/var/run/shortinette.sock") {
+		stream.write_all(text.as_bytes()).ok();
+	}
+}
+
+fn num_days_in_month_helper(year: u32, month: u32) {
+	let text = format!("num_days_in_month({}, {});\n", year, month);
+	if let Ok(mut stream) = UnixStream::connect("/var/run/shortinette.sock") {
+		stream.write_all(text.as_bytes()).ok();
+	}
+}
+`
 
 const CargoTestEx05 = `
 #[cfg(test)]
@@ -73,10 +95,103 @@ mod shortinette_tests_rust_0005 {
 	#[test]
     #[should_panic]
     fn test_invalid_month_2() {
+        num_days_in_month(2001, 13);
+    }
+
+	#[test]
+    #[should_panic]
+    fn test_invalid_month_3() {
         num_days_in_month(0, 2);
     }
 }
 `
+
+func runUnixSocket(ctx context.Context, output *strings.Builder, ready chan<- bool) {
+	socketPath := `/var/run/shortinette.sock`
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return
+	}
+	defer os.Remove(socketPath)
+	defer listener.Close()
+	ready <- true
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			conn, err := listener.Accept()
+			if err != nil {
+				if ctx.Err() != nil {
+					return
+				}
+				continue
+			}
+			go func(conn net.Conn) {
+				defer conn.Close()
+				reader := bufio.NewReader(conn)
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					default:
+						line, err := reader.ReadString('\n')
+						if err != nil {
+							if ctx.Err() != nil {
+								return
+							}
+							break
+						}
+						output.WriteString(line)
+					}
+				}
+			}(conn)
+		}
+	}
+}
+
+func addHelperToFunction(exercise *Exercise.Exercise, function string, helper string) error {
+	content, err := os.ReadFile(exercise.TurnInFiles[0])
+	if err != nil {
+		return err
+	}
+	re, err := regexp.Compile(`fn\s` + function + `\b`)
+	if err != nil {
+		return err
+	}
+	total := 0
+	for {
+		result := re.FindAllIndex(content, -1)
+		if len(result) == total {
+			if total == 0 {
+				return nil
+			}
+			return os.WriteFile(exercise.TurnInFiles[0], content, 0644)
+		}
+		match := result[total]
+		for pos := match[1]; pos < len(content); pos++ {
+			if content[pos] == '{' {
+				pos++
+				content = append(content[:pos], append([]byte(helper), content[pos:]...)...)
+				total++
+				break
+			}
+		}
+	}
+}
+
+func appendHelperFunctions(exercise *Exercise.Exercise) error {
+	if err := addHelperToFunction(exercise, "is_leap_year", `is_leap_year_helper(year);`); err != nil {
+		return err
+	}
+	if err := addHelperToFunction(exercise, "num_days_in_month", `num_days_in_month_helper(year, month);`); err != nil {
+		return err
+	}
+	if err := testutils.AppendStringToFile(TestExistenceHelper, exercise.TurnInFiles[0]); err != nil {
+		return err
+	}
+	return nil
+}
 
 func missingTests(message string) Exercise.Result {
 	return Exercise.Result{
@@ -86,10 +201,14 @@ func missingTests(message string) Exercise.Result {
 }
 
 func monthTestExistenceTest(content string) Exercise.Result {
-	leapYearsFound := make(map[uint64]bool)
-	commonYearsFound := make(map[uint64]bool)
-	invalidFound := false
+	leapYearsFound := make(map[uint32]bool)
+	commonYearsFound := make(map[uint32]bool)
+	invalidMonthFound := false
 
+	for month := uint32(1); month <= 12; month++ {
+		leapYearsFound[month] = false
+		commonYearsFound[month] = false
+	}
 	pattern := `num_days_in_month\((\d+),\s*(\d+)\)`
 	regex := regexp.MustCompile(pattern)
 	matches := regex.FindAllStringSubmatch(content, -1)
@@ -103,18 +222,18 @@ func monthTestExistenceTest(content string) Exercise.Result {
 			if err != nil {
 				return Exercise.InternalError(err.Error())
 			}
-			if month < 1 || month > 12 || year == 0 {
-				invalidFound = true
+			if month < 1 || month > 12 {
+				invalidMonthFound = true
 			} else {
-				if year%4 != 0 || (year%100 == 0 && year%400 != 0) {
-					commonYearsFound[month] = true
+				if year > 0 && (year%4 != 0 || (year%100 == 0 && year%400 != 0)) {
+					commonYearsFound[uint32(month)] = true
 				} else {
-					leapYearsFound[month] = true
+					leapYearsFound[uint32(month)] = true
 				}
 			}
 		}
 	}
-	if !invalidFound {
+	if !invalidMonthFound {
 		return missingTests("no test for invalid month")
 	}
 	for _, found := range leapYearsFound {
@@ -131,10 +250,20 @@ func monthTestExistenceTest(content string) Exercise.Result {
 }
 
 func testExistenceTest(exercise *Exercise.Exercise) Exercise.Result {
-	content, err := os.ReadFile(exercise.TurnInFiles[0])
-	if err != nil {
-		return Exercise.InternalError(fmt.Sprintf("error reading file: %v", err.Error()))
+	if err := appendHelperFunctions(exercise); err != nil {
+		return Exercise.InternalError(err.Error())
 	}
+	var output strings.Builder
+	ctx, cancel := context.WithCancel(context.Background())
+	ready := make(chan bool)
+	defer cancel()
+	go runUnixSocket(ctx, &output, ready)
+	<-ready
+	if result := cargo.CargoTest(exercise, 500*time.Millisecond, []string{}); !result.Passed {
+		return result
+	}
+	cancel()
+	content := output.String()
 	if !strings.Contains(string(content), "is_leap_year(1600)") || !strings.Contains(string(content), "is_leap_year(1500)") {
 		return missingTests("missing test(s) for is_leap_year function")
 	}
@@ -174,29 +303,22 @@ func ex05Test(exercise *Exercise.Exercise) Exercise.Result {
 	if err := alloweditems.Check(*exercise, "", map[string]int{"unsafe": 0}); err != nil {
 		return Exercise.CompilationError(err.Error())
 	}
-	/*
-		if result := testExistenceTest(exercise); !result.Passed {
-			return result
-		}
-	*/
-	workingDirectory := filepath.Join(exercise.CloneDirectory, exercise.TurnInDirectory)
-	_, err := testutils.RunCommandLine(workingDirectory, "cargo", []string{"test"})
-	if err != nil {
-		logger.Exercise.Printf("%v", err)
-		return Exercise.Result{Passed: false, Output: err.Error()}
+	if result := cargo.CargoTest(exercise, 500*time.Millisecond, []string{}); !result.Passed {
+		return result
+	}
+	if result := testExistenceTest(exercise); !result.Passed {
+		return result
 	}
 	if err := testutils.AppendStringToFile(CargoTestEx05, exercise.TurnInFiles[0]); err != nil {
 		return Exercise.InternalError(err.Error())
 	}
-	_, err = testutils.RunCommandLine(workingDirectory, "cargo", []string{"test"})
-	if err != nil {
-		logger.Exercise.Printf("%v", err)
-		return Exercise.Result{Passed: false, Output: err.Error()}
+	if result := cargo.CargoTest(exercise, 500*time.Millisecond, []string{}); !result.Passed {
+		return result
 	}
+	workingDirectory := filepath.Join(exercise.CloneDirectory, exercise.TurnInDirectory)
 	output, err := testutils.RunCommandLine(workingDirectory, "cargo", []string{"run"})
 	if err != nil {
-		logger.Exercise.Printf("%v", err)
-		return Exercise.Result{Passed: false, Output: err.Error()}
+		return Exercise.RuntimeError(err.Error())
 	}
 	expectedOutput := expectedMainOutput()
 	if output != expectedOutput {
