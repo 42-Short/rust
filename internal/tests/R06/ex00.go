e9440ad627bb1017e672c02a31ed487cd7e99294
winstonallo <arthurbiedchar@gmail.com>
2024-09-21 15:45:31 +0200

    add: ex00.rs

--- internal/tests/R06/ex00.go
+++ internal/tests/R06/ex00.go
@@ -1,6 +1,7 @@
 package R06
 
 import (
+	"os"
 	"path/filepath"
 	"rust-piscine/internal/alloweditems"
 
@@ -8,64 +9,6 @@ import (
 	"github.com/42-Short/shortinette/pkg/logger"
 	"github.com/42-Short/shortinette/pkg/testutils"
 )
-
-var tests00 = `
-#[cfg(test)]
-mod shortinette_tests_rust_0600 {
-    use super::*;
-
-    #[test]
-    fn swap_basic_type() {
-        let mut a = 69;
-        let mut b = 420;
-        ft_swap(&mut a, &mut b);
-        assert_eq!(a, 420);
-        assert_eq!(b, 69);
-    }
-
-    #[test]
-    fn swap_complex_type() {
-        let mut a = String::from("Hello, World!");
-        let mut b = String::from("Goodbye, World!");
-        ft_swap(&mut a, &mut b);
-        assert_eq!(a, "Goodbye, World!");
-        assert_eq!(b, "Hello, World!");
-    }
-
-    #[test]
-    fn swap_zero_sized_type() {
-        let mut a = ();
-        let mut b = ();
-        ft_swap(&mut a, &mut b);
-        assert_eq!(a, ());
-        assert_eq!(b, ());
-    }
-
-    #[test]
-    fn strlen() {
-        let s = b"Hello, World!\0";
-        let len = unsafe { ft_strlen(s.as_ptr()) };
-        assert_eq!(len, 13);
-    }
-
-    #[test]
-    fn strcpy() {
-        let s = b"Hello, World!\0";
-        let mut dst = [0u8; 14];
-        unsafe { ft_strcpy(dst.as_mut_ptr(), s.as_ptr()) };
-        assert_eq!(&dst, s);
-    }
-
-    #[test]
-    fn strcpy_insufficient_buffer() {
-        let s = b"Hello, World!\0";
-        let mut dst = [0u8; 5];
-        unsafe { ft_strcpy(dst.as_mut_ptr(), s.as_ptr()) };
-        let dst_str = std::str::from_utf8(&dst).expect("Invalid UTF-8 sequence");
-        assert_eq!(dst_str, "Hello");
-    }
-}
-`
 
 var clippyTomlAsString00 = `
 disallowed-methods = ["std::mem::replace", "std::mem::take", "std::mem::swap", "std::ptr::swap_nonoverlapping", "std::mem::size_of", "std::mem::align_of", "std::ptr::copy", "std::ptr::copy_nonoverlapping", "core::mem::replace", "core::mem::take", "core::mem::swap", "core::ptr::swap_nonoverlapping", "core::mem::size_of", "core::mem::align_of", "core::ptr::copy", "core::ptr::copy_nonoverlapping"]
@@ -75,12 +18,16 @@ func ex00Test(exercise *Exercise.Exercise) Exercise.Result {
 	if err := alloweditems.Check(*exercise, clippyTomlAsString00, nil); err != nil {
 		return Exercise.CompilationError(err.Error())
 	}
+    tests00, err := os.ReadFile("internal/tests/R06/ex00.rs")
+    if err != nil {
+        return Exercise.InternalError(err.Error())
+    }
 	workingDirectory := filepath.Join(exercise.CloneDirectory, exercise.TurnInDirectory)
-	if err := testutils.AppendStringToFile(tests00, exercise.TurnInFiles[0]); err != nil {
+	if err := testutils.AppendStringToFile(string(tests00), exercise.TurnInFiles[0]); err != nil {
 		logger.Exercise.Printf("could not write to %s: %v", exercise.TurnInFiles[0], err)
 		return Exercise.InternalError(err.Error())
 	}
-	_, err := testutils.RunCommandLine(workingDirectory, "cargo", []string{"valgrind", "test"})
+	_, err = testutils.RunCommandLine(workingDirectory, "cargo", []string{"valgrind", "test"})
 	if err != nil {
 		return Exercise.RuntimeError(err.Error())
 	}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"rust-piscine/internal/scheduler"
 	"rust-piscine/internal/tests/R00"
 	"rust-piscine/internal/tests/R01"
+	"rust-piscine/internal/tests/R03"
 	"time"
 
 	Module "github.com/42-Short/shortinette/pkg/interfaces/module"
@@ -16,10 +17,10 @@ func main() {
 	modules := map[string]Module.Module{
 		"00": *R00.R00(),
 		"01": *R01.R01(),
-		// TODO: "02": *R02.R02(), // TODO
-		// TODO: "03": *R03.R03(), // TODO
+		"02": *R02.R02(), // TODO
+		"03": *R03.R03(), // TODO
 		// "04": *R04.R04(),
-		// TODO: "05": *R05.R05(), // TODO
+		"05": *R05.R05(), // TODO
 		// "06": *R06.R06(),
 	}
 	short := Short.NewShort("Rust Piscine 1.0", modules, webhook.NewWebhookTestMode(modules, "/webhook", "8080"))
