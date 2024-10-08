507fae33053d07e5f59432de190582cf48ea45b9
Simon Dablander <sdabland@student.42vienna.com>
2024-10-08 16:31:58 +0200

    feat(R01): `ex04` print failed test

--- internal/tests/R01/ex04.go
+++ internal/tests/R01/ex04.go
@@ -18,63 +18,63 @@ mod shortinette_tests_rust_0104 {
 	fn test_empty() {
 		let mut boxes = [];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes.len(), 0);
+		assert_eq!(boxes.len(), 0, "Failed for an empty list as input");
 	}
 
 	#[test]
 	fn test_datatype() {
 		let mut boxes = [[1u32, 1u32], [2u32, 2u32]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[2u32, 2u32], [1u32, 1u32]]);
+		assert_eq!(boxes, [[2u32, 2u32], [1u32, 1u32]], "Failed ordering [[1, 1], [2, 2]]");
 	}
 
 	#[test]
 	fn test_0() {
 		let mut boxes = [[2, 2], [1, 1], [3, 3]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[3, 3], [2, 2], [1, 1]]);
+		assert_eq!(boxes, [[3, 3], [2, 2], [1, 1]], "Failed ordering [[2, 2], [1, 1], [3, 3]]");
 	}
 
 	#[test]
 	fn test_1() {
 		let mut boxes = [[0, 0], [1, 1], [1, 1], [0, 0]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[1, 1], [1, 1], [0, 0], [0, 0]]);
+		assert_eq!(boxes, [[1, 1], [1, 1], [0, 0], [0, 0]], "Failed ordering [[0, 0], [1, 1], [1, 1], [0, 0]]");
 	}
 
 	#[test]
 	fn test_2() {
 		let mut boxes = [[0, 1], [1, 1]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[1, 1], [0, 1]]);
+		assert_eq!(boxes, [[1, 1], [0, 1]], "Failed ordering [[0, 1], [1, 1]]");
 	}
 
 	#[test]
 	fn test_3() {
 		let mut boxes = [[1, 0], [1, 1]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[1, 1], [1, 0]]);
+		assert_eq!(boxes, [[1, 1], [1, 0]], "Failed ordering [[1, 0], [1, 1]]");
 	}
 
 	#[test]
 	fn test_4() {
 		let mut boxes = [[1, 1], [2, 1]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[2, 1], [1, 1]]);
+		assert_eq!(boxes, [[2, 1], [1, 1]], "Failed ordering [[1, 1], [2, 1]]");
 	}
 
 	#[test]
 	fn test_5() {
 		let mut boxes = [[1, 1], [1, 2]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[1, 2], [1, 1]]);
+		assert_eq!(boxes, [[1, 2], [1, 1]], "Failed ordering [[1, 1], [1, 2]]");
 	}
 
 	#[test]
 	fn test_6() {
 		let mut boxes = [[5, 3], [5, 2], [8, 5], [2, 2], [1, 1], [2, 1]];
 		sort_boxes(&mut boxes);
-		assert_eq!(boxes, [[8, 5], [5, 3], [5, 2], [2, 2], [2, 1], [1, 1]]);
+		assert_eq!(boxes, [[8, 5], [5, 3], [5, 2], [2, 2], [2, 1], [1, 1]], "Failed ordering [[5, 3], [5, 2], [8, 5], [2, 2], [1, 1], [2, 1]]");
 	}
 
 	#[test]
@@ -82,6 +82,7 @@ mod shortinette_tests_rust_0104 {
 	fn test_7() {
 		let mut boxes = [[5, 3], [5, 2], [8, 5], [2, 2], [1, 2], [2, 1]];
 		sort_boxes(&mut boxes);
+		eprintln!("Did not panic for [[5, 3], [5, 2], [8, 5], [2, 2], [1, 2], [2, 1]]");
 	}
 
 	#[test]
@@ -89,6 +90,7 @@ mod shortinette_tests_rust_0104 {
 	fn test_8() {
 		let mut boxes = [[2, 1], [5, 3], [5, 2], [1, 2], [8, 5], [2, 2]];
 		sort_boxes(&mut boxes);
+		eprintln!("Did not panic for [[2, 1], [5, 3], [5, 2], [1, 2], [8, 5], [2, 2]]");
 	}
 }
 `
