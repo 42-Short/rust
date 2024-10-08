493136c7dfa9cfbcadf83c876a8b9128060be6da
Simon Dablander <sdabland@student.42vienna.com>
2024-10-08 21:31:08 +0200

    fix(R01): send test case in trace

--- internal/tests/R01/ex06.go
+++ internal/tests/R01/ex06.go
@@ -61,57 +61,57 @@ mod shortinette_tests_rust_0106 {
 
 	#[test]
 	fn test_0() {
-		assert_eq!(big_add(b"1", b"2"), b"3");
+		assert_eq!(big_add(b"1", b"2"), b"3", r#"Failed for (b"1", b"2")"#);
 	}
 
 	#[test]
 	fn test_1() {
-		assert_eq!(big_add(b"8", b"4"), b"12");
+		assert_eq!(big_add(b"8", b"4"), b"12", r#"Failed for (b"8", b"4")"#);
 	}
 
 	#[test]
 	fn test_2() {
-		assert_eq!(big_add(b"0", b"0"), b"0");
+		assert_eq!(big_add(b"0", b"0"), b"0", r#"Failed for (b"0", b"0")"#);
 	}
 
 	#[test]
 	fn test_3() {
-		assert_eq!(big_add(b"00003674", b"1757"), b"5431");
+		assert_eq!(big_add(b"00003674", b"1757"), b"5431", r#"Failed for (b"00003674", b"1757")"#);
 	}
 
 	#[test]
 	fn test_4() {
-		assert_eq!(big_add(b"3319", b"00001259"), b"4578");
+		assert_eq!(big_add(b"3319", b"00001259"), b"4578", r#"Failed for (b"3319", b"00001259")"#);
 	}
 
 	#[test]
 	fn test_5() {
-		assert_eq!(big_add(b"42", b"57"), b"99");
+		assert_eq!(big_add(b"42", b"57"), b"99", r#"Failed for (b"42", b"57")"#);
 	}
 
 	#[test]
 	fn test_6() {
-		assert_eq!(big_add(b"42", b"58"), b"100");
+		assert_eq!(big_add(b"42", b"58"), b"100", r#"Failed for (b"42", b"58")"#);
 	}
 
 	#[test]
 	fn test_7() {
-		assert_eq!(big_add(b"42", b"59"), b"101");
+		assert_eq!(big_add(b"42", b"59"), b"101", r#"Failed for (b"42", b"59")"#);
 	}
 
 	#[test]
 	fn test_8() {
-		assert_eq!(big_add(b"50000", b"50000"), b"100000");
+		assert_eq!(big_add(b"50000", b"50000"), b"100000", r#"Failed for (b"50000", b"50000")"#);
 	}
 
 	#[test]
 	fn test_9() {
-		assert_eq!(big_add(b"340282366920938463463374607431768211456", b"18446744073709551616"), b"340282366920938463481821351505477763072");
+		assert_eq!(big_add(b"340282366920938463463374607431768211456", b"18446744073709551616"), b"340282366920938463481821351505477763072", r#"Failed for (b"340282366920938463463374607431768211456", b"18446744073709551616")"#);
 	}
 
 	#[test]
 	fn test_10() {
-		assert_eq!(big_add(b"000000", b"00000"), b"0");
+		assert_eq!(big_add(b"000000", b"00000"), b"0", r#"Failed for (b"000000", b"00000")"#);
 	}
 }
 `
